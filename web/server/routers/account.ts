871b834699b7500613038de2c0b0687cf04b8a6f
Tejas Mehta <tmthecoder@gmail.com>
2023-01-28 15:55:52 -0500

    feat: add account removal route

--- web/server/routers/account.ts
+++ web/server/routers/account.ts
@@ -1,2 +1,42 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { removeAccount } from "../../db/actions/Account";
+import { updateUser } from "../../db/actions/User";
+import Account from "../../db/models/Account";
 import { router, protectedProcedure } from "../trpc";
-export const accountRouter = router({});
+
+export const accountRouter = router({
+  remove: protectedProcedure
+    .input(
+      z.object({
+        email: z.string().email("Invalid email provided"),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const email = input.email;
+      if (ctx.session?.email === email)
+        throw new TRPCError({
+          message: "Unauthorized - Cannot remove own account",
+          code: "UNAUTHORIZED",
+        });
+
+      const session = await Account.startSession();
+      session.startTransaction();
+
+      try {
+        await removeAccount(email, session);
+        await updateUser(email, { disabled: true }, session);
+
+        session.commitTransaction();
+
+        return { success: true };
+      } catch (e) {
+        session.abortTransaction();
+
+        throw new TRPCError({
+          message: "Internal Server Error",
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+    }),
+});
