f70ca08111180bdb49d59a110ec761fe2c3df937
mhartlage3 <madeline.hartlage@gatech.edu>
2023-02-04 16:29:46 -0500

    rendered logout button

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,6 +2,8 @@ import {
   FacebookAuthProvider,
   GoogleAuthProvider,
   signInWithPopup,
+  signOut,
+  User,
 } from "firebase/auth";
 import {
   Button,
@@ -15,152 +17,167 @@ import {
   Link,
 } from "@chakra-ui/react";
 import { auth } from "../utils/firebase/firebaseClient";
-import { trpc } from "../utils/trpc";
-import { useAuth } from "../context/auth";
+import React from "react";
+//import { trpc } from "../utils/trpc";
+//import { useAuth } from "../context/auth";
 
 export default function Home() {
+  const [user, setUser] = React.useState<User | null>(null);
+
   async function handleLoginFacebook() {
     const provider = new FacebookAuthProvider();
     const result = await signInWithPopup(auth, provider);
-    const user = result.user;
+    setUser(result.user);
   }
 
   async function handleLoginGoogle() {
     const provider = new GoogleAuthProvider();
     const result = await signInWithPopup(auth, provider);
-    const user = result.user;
+    setUser(result.user);
   }
 
-  const { user } = useAuth();
-  console.log(user);
+  //const { user } = useAuth();
+  //console.log(user);
 
-  const listPosts = trpc.post.list.useQuery();
-  console.log(listPosts.data);
+  //const listPosts = trpc.post.list.useQuery();
+  //console.log(listPosts.data);
 
   return (
     <Flex height="100vh">
-      <Stack direction="row" width="100%" height="100%">
-        <Flex bgColor="#D9D9D9" width="50%"></Flex>
-        <Flex bgColor="#FFFFFF" width="50%" justifyContent="center">
-          <Stack direction="column" width="100%" alignItems="center">
-            <Stack
-              direction="row"
-              justifyContent="flex-end"
-              width="100%"
-              padding="2%"
-              marginBottom="15%"
-            >
-              <Flex
-                bgColor="#D9D9D9"
-                paddingRight="3%"
-                paddingLeft="3%"
-                paddingTop="1%"
-                paddingBottom="1%"
-              >
-                logo
-              </Flex>
-            </Stack>
-            <Stack
-              direction="column"
-              alignItems="flex-start"
-              width="35%"
-              spacing="10"
-            >
-              <Text
-                color="#000000"
-                fontWeight="bold"
-                fontSize="4xl"
-                lineHeight="10"
-              >
-                Welcome to the page message!
-              </Text>
-              <Text color="#000000" fontWeight="semibold" fontSize="lg">
-                Subtitle and further description of the site.
-              </Text>
-              <Button
-                bgColor="#D9D9D9"
+      {user === null ? (
+        <Stack direction="row" width="100%" height="100%">
+          <Flex bgColor="#D9D9D9" width="50%"></Flex>
+          <Flex bgColor="#FFFFFF" width="50%" justifyContent="center">
+            <Stack direction="column" width="100%" alignItems="center">
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
                 width="100%"
-                borderRadius="16px"
-                onClick={handleLoginFacebook}
+                padding="2%"
+                marginBottom="15%"
               >
-                continue with facebook
-              </Button>
-              <Stack direction="row" width="100%" alignItems="center">
-                <Divider width="45%" border="1px solid black"></Divider>
-                <Text>or</Text>
-                <Divider width="45%" border="1px solid black"></Divider>
+                <Flex
+                  bgColor="#D9D9D9"
+                  paddingRight="3%"
+                  paddingLeft="3%"
+                  paddingTop="1%"
+                  paddingBottom="1%"
+                >
+                  logo
+                </Flex>
               </Stack>
-              <Button
-                bgColor="#D9D9D9"
-                width="100%"
-                borderRadius="16px"
-                onClick={handleLoginGoogle}
+              <Stack
+                direction="column"
+                alignItems="flex-start"
+                width="35%"
+                spacing="10"
               >
-                continue with Google
-              </Button>
-            </Stack>
-            <Stack
-              direction="row"
-              width="100%"
-              alignItems="center"
-              justifyContent="flex-end"
-              paddingTop="15%"
-              paddingRight="2%"
-            >
-              <Text color="#6D6D6D" fontSize="small">
-                what is this site?
-              </Text>
-              <Popover>
-                <PopoverTrigger>
-                  <Button
-                    bgColor="#D9D9D9"
-                    borderRadius="100%"
-                    size="xs"
-                    color="#6D6D6D"
-                  >
-                    ?
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent marginRight={10}>
-                  <Flex background={"rgba(217, 217, 217, 0.5)"} padding="5%">
-                    <Stack
-                      direction="column"
-                      fontWeight="medium"
-                      lineHeight="5"
-                      spacing={4}
+                <Text
+                  color="#000000"
+                  fontWeight="bold"
+                  fontSize="4xl"
+                  lineHeight="10"
+                >
+                  Welcome to the page message!
+                </Text>
+                <Text color="#000000" fontWeight="semibold" fontSize="lg">
+                  Subtitle and further description of the site.
+                </Text>
+                <Button
+                  bgColor="#D9D9D9"
+                  width="100%"
+                  borderRadius="16px"
+                  onClick={handleLoginFacebook}
+                >
+                  continue with facebook
+                </Button>
+                <Stack direction="row" width="100%" alignItems="center">
+                  <Divider width="45%" border="1px solid black"></Divider>
+                  <Text>or</Text>
+                  <Divider width="45%" border="1px solid black"></Divider>
+                </Stack>
+                <Button
+                  bgColor="#D9D9D9"
+                  width="100%"
+                  borderRadius="16px"
+                  onClick={handleLoginGoogle}
+                >
+                  continue with Google
+                </Button>
+              </Stack>
+              <Stack
+                direction="row"
+                width="100%"
+                alignItems="center"
+                justifyContent="flex-end"
+                paddingTop="15%"
+                paddingRight="2%"
+              >
+                <Text color="#6D6D6D" fontSize="small">
+                  what is this site?
+                </Text>
+                <Popover>
+                  <PopoverTrigger>
+                    <Button
+                      bgColor="#D9D9D9"
+                      borderRadius="100%"
+                      size="xs"
+                      color="#6D6D6D"
                     >
-                      <Text>
-                        The purpose of this site is to provide a space for
-                        confirmed Angels Among Us fosters to get matched with
-                        pets in need.
-                      </Text>
-                      <Text>
-                        If you have not yet applied to become a foster, fill out{" "}
-                        <Link>
-                          <Text as="span" color="#0085FF">
-                            {" "}
-                            this application{" "}
-                          </Text>
-                        </Link>
-                        to apply.
-                      </Text>
-                      <Text>
-                        For any other questions or concerns please email{" "}
-                        <Link>
-                          <Text as="span" color="#0085FF">
-                            person@aau.com.
-                          </Text>
-                        </Link>
-                      </Text>
-                      <Text>Thanks!</Text>
-                    </Stack>
-                  </Flex>
-                </PopoverContent>
-              </Popover>
+                      ?
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent marginRight={10}>
+                    <Flex background={"rgba(217, 217, 217, 0.5)"} padding="5%">
+                      <Stack
+                        direction="column"
+                        fontWeight="medium"
+                        lineHeight="5"
+                        spacing={4}
+                      >
+                        <Text>
+                          The purpose of this site is to provide a space for
+                          confirmed Angels Among Us fosters to get matched with
+                          pets in need.
+                        </Text>
+                        <Text>
+                          If you have not yet applied to become a foster, fill
+                          out{" "}
+                          <Link>
+                            <Text as="span" color="#0085FF">
+                              {" "}
+                              this application{" "}
+                            </Text>
+                          </Link>
+                          to apply.
+                        </Text>
+                        <Text>
+                          For any other questions or concerns please email{" "}
+                          <Link>
+                            <Text as="span" color="#0085FF">
+                              person@aau.com.
+                            </Text>
+                          </Link>
+                        </Text>
+                        <Text>Thanks!</Text>
+                      </Stack>
+                    </Flex>
+                  </PopoverContent>
+                </Popover>
+              </Stack>
             </Stack>
-          </Stack>
-        </Flex>
-      </Stack>
+          </Flex>
+        </Stack>
+      ) : (
+        <Button
+          onClick={() => {
+            signOut(auth);
+            setUser(null);
+          }}
+        >
+          Logout
+        </Button>
+      )}
     </Flex>
   );
 }
