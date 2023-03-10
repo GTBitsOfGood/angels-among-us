08f2aa3c0792a64132fcc4428545b81294d35bf2
Joanna Cheng <joannacheng21@outlook.com>
2023-03-10 17:39:26 -0500

    basic framework for feed

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -23,6 +23,16 @@ import {
   IconButton,
   Spinner,
   Center,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  Box,
+  AccordionPanel,
+  AccordionIcon,
+  Card,
+  CardHeader,
+  Checkbox,
+  CardBody,
 } from "@chakra-ui/react";
 import { auth } from "../utils/firebase/firebaseClient";
 import { useAuth } from "../context/auth";
@@ -51,7 +61,7 @@ export default function Home() {
   }
 
   if (authorized) {
-    return (
+    /*return (
       <Flex height="100vh">
         <Flex width="100%" justifyContent="center" alignItems="center">
           <Button
@@ -68,6 +78,141 @@ export default function Home() {
             onClose={onClose}
           />
         </Flex>
+      </Flex>
+    );*/
+
+    return (
+      <Flex height="100vh" backgroundColor="#DFDFDF">
+        <Stack
+          spacing={8}
+          direction="row"
+          marginTop="100px"
+          marginBottom="50px"
+          marginX="40px"
+        >
+          <Flex
+            width="500px"
+            borderRadius="10px"
+            backgroundColor="#FFFFFF"
+            direction="column"
+          >
+            <Text fontWeight="semibold" margin="16px">
+              Filter By:
+            </Text>
+            <Flex justifyContent="flex-end" margin="12px" gap="8px">
+              <Button
+                backgroundColor="#FFFFFF"
+                fontWeight="normal"
+                color="#7D7E82"
+                borderWidth="1px"
+                borderColor="#7D7E82"
+                borderRadius="12px"
+              >
+                Clear All
+              </Button>
+              <Button
+                backgroundColor="#8F9294"
+                fontWeight="normal"
+                color="#FFFFFF"
+                borderRadius="12px"
+              >
+                Use My Preferences
+              </Button>
+            </Flex>
+            <Accordion allowMultiple={true}>
+              <AccordionItem>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    <Text fontWeight="semibold">General Information</Text>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel pb={4} marginRight="40px">
+                  <Text>Which types of fosters can you help with?</Text>
+                  <Flex
+                    direction="column"
+                    borderWidth="2px"
+                    borderColor="#D9D9D9"
+                    borderRadius="6px"
+                    gap="10px"
+                    padding="16px"
+                    marginTop="16px"
+                  >
+                    <Checkbox>Return</Checkbox>
+                    <Checkbox>Boarding</Checkbox>
+                    <Checkbox>Temporary</Checkbox>
+                    <Checkbox>Foster Move</Checkbox>
+                    <Checkbox>Shelter</Checkbox>
+                    <Checkbox>Owner Surrender</Checkbox>
+                  </Flex>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </Flex>
+          <Flex
+            width="900px"
+            borderRadius="10px"
+            backgroundColor="#C1C1C1"
+            direction="column"
+            alignItems="center"
+          >
+            <Text marginTop="20px" fontWeight="bold" fontSize="18px">
+              Latest Posts
+            </Text>
+            <Card margin="20px" width="95%" padding="16px">
+              <Flex gap="20px">
+                <Image
+                  width="150px"
+                  height="150px"
+                  backgroundColor="#D9D9D9"
+                  borderRadius="10px"
+                ></Image>
+                <Flex
+                  direction="column"
+                  width="80%"
+                  marginTop="8px"
+                  marginRight="20px"
+                >
+                  <Text fontSize="14px">MM/DD/YYYY XX:XX PM</Text>
+                  <Text
+                    margin="0px"
+                    paddingY="0px"
+                    fontWeight="bold"
+                    fontSize="18px"
+                  >
+                    Pet Name
+                  </Text>
+                  <Text
+                    margin="0px"
+                    backgroundColor="#C6E3F9"
+                    width="fit-content"
+                    paddingX="16px"
+                    paddingY="4px"
+                    borderRadius="20px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                    fontSize="15px"
+                    fontWeight="semibold"
+                  >
+                    Foster Move
+                  </Text>
+                  <Text fontSize="14px" lineHeight="18px" color="#656565">
+                    Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor
+                    sit amet consectetur. Lorem ipsum dolor sit amet
+                    consectetur.
+                  </Text>
+                </Flex>
+              </Flex>
+            </Card>
+            <Card>
+              <CardHeader>Pet Name</CardHeader>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit
+                amet consectetur. Lorem ipsum dolor sit amet consectetur.
+              </Text>
+            </Card>
+          </Flex>
+        </Stack>
       </Flex>
     );
   }
