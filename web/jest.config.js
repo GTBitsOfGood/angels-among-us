9d8672c023dfca953b38fe96546bdbedaf75afdb
Katsuki Chan <katsukidotxyz@gmail.com>
2023-08-16 14:55:22 -0500

    fix: jest coverage threhold

--- web/jest.config.js
+++ web/jest.config.js
@@ -12,6 +12,14 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   preset: "@shelf/jest-mongodb",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
