d5ad26ff06be7d938b86f0ceb4245f7ee0ce4ecf
Katsuki Chan <katsukidotxyz@gmail.com>
2023-09-22 17:25:46 -0400

    Fix feed post default icon on no valid attachments (#134)
    * fix: default dog icon on no image attachments
    
    * fix: feed posts not taking up full width

--- web/components/Feed/Feed.tsx
+++ web/components/Feed/Feed.tsx
@@ -459,7 +459,6 @@ function Feed(props: {
         marginBottom="50px"
         marginX={{ base: "0px", lg: "40px" }}
         direction={{ base: "column", lg: "row" }}
-        flex="1"
       >
         <Flex
           display={{ base: "flex", lg: "none" }}
