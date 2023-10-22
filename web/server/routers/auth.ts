f4e6acd1479384c6fc41ddb3b3d9a424c6068d8d
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-10-22 12:56:07 -0400

    Style updated, added spinner when searching

--- web/server/routers/auth.ts
+++ web/server/routers/auth.ts
@@ -29,7 +29,6 @@ export const authRouter = router({
             message: "Not authenticated",
           });
         }
-
         const user = await findUserByEmail(ctx.session.email);
         const account = await findAccount(ctx.session.email);
 
@@ -38,6 +37,8 @@ export const authRouter = router({
           const document = await updateUserByUid(ctx.session.uid, {
             role: account.role,
             disabled: false,
+            name: ctx.session.name,
+            picture: ctx.session.picture,
           });
 
           return {
