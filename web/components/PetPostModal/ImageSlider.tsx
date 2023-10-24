31b367c72a92c96355f776ad9521e2420e845e3c
lmcpeters3 <lmcpeters3@gatech.edu>
2023-10-24 13:22:34 -0400

    add video previews

--- web/components/PetPostModal/ImageSlider.tsx
+++ web/components/PetPostModal/ImageSlider.tsx
@@ -21,8 +21,8 @@ function ImageSlider(props: { attachments: Array<string> }) {
             bottom={0}
             left={0}
             p={3}
-            alignItems="center"
-            maxHeight={["50vh", "65vh"]}
+            alignItems="flex-end"
+            maxHeight={["23.5vh", "35.5vh"]}
           >
             <Flex onClick={clickHandler} cursor="pointer">
               <FaArrowCircleLeft size={30} />
@@ -40,8 +40,8 @@ function ImageSlider(props: { attachments: Array<string> }) {
             bottom={0}
             right={0}
             p={3}
-            alignItems="center"
-            maxHeight={["50vh", "65vh"]}
+            alignItems="flex-end"
+            maxHeight={["23.5vh", "35.5vh"]}
           >
             <Flex onClick={clickHandler} cursor="pointer">
               <FaArrowCircleRight size={30} />
@@ -50,28 +50,49 @@ function ImageSlider(props: { attachments: Array<string> }) {
         );
       }}
     >
-      {/* Implement video previews */}
       {props.attachments.length > 0
         ? props.attachments?.map((slide) => {
             return (
               <Flex
                 justifyContent={"center"}
                 alignItems={"center"}
-                bgColor={"tag-primary-bg"}
+                bgColor={
+                  slide.slice(slide.length - 3) === "mp4"
+                    ? "black"
+                    : "tag-primary-bg"
+                }
                 borderRadius="15px"
                 overflow={"hidden"}
-                minHeight={["40vh", "65vh"]}
+                height={["40vh", "65vh"]}
                 key={slide}
                 paddingX={0}
               >
-                <Image
-                  src={slide}
-                  objectFit={"cover"}
-                  verticalAlign={"true"}
-                  align="center"
-                  maxHeight={["40vh", "65vh"]}
-                  alt=""
-                />
+                {slide.slice(slide.length - 3) === "mp4" ? (
+                  <Flex
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    height={["40vh", "65vh"]}
+                    width={["94%", "96%"]}
+                    marginTop={10}
+                  >
+                    <iframe
+                      src={slide}
+                      key={slide}
+                      height={"100%"}
+                      width={"100%"}
+                    ></iframe>
+                  </Flex>
+                ) : (
+                  <Image
+                    src={slide}
+                    key={slide}
+                    objectFit={"cover"}
+                    verticalAlign={"true"}
+                    align="center"
+                    maxHeight={["40vh", "65vh"]}
+                    alt=""
+                  />
+                )}
               </Flex>
             );
           })
