3ca9d2c83182e6159ec2251d1d15b8c742118529
Edmund Xin <edmundxin02@gmail.com>
2023-11-14 01:28:30 -0500

    Update cors headers in next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -10,6 +10,27 @@ const nextConfig = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 // Injected content via Sentry wizard below
